f610d34df2e168a4fc63f304085c67208f73811e
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2020-04-04 17:06:41 +0200

    add C89 implementation

---
+++ c/nn.h
@@ -0,0 +1,250 @@
+/*
+ * Copyright 2020 Sergii Zaitsev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef NN_H
+#define NN_H
+
+#include <assert.h>
+#include <math.h>
+#include <stdlib.h>
+
+struct nn_vec {
+  size_t len;
+  float *data;
+};
+
+#define NN_FLAG_NO_BIAS 1
+
+#define NN_FLAG_LINEAR (0 << 1)
+#define NN_FLAG_RELU (1 << 1)
+#define NN_FLAG_SIGMOID (2 << 1)
+#define NN_ACTFN (3 << 1)
+
+struct nn_layer {
+  unsigned int flags;
+  struct nn_vec input;
+  struct nn_vec output;
+  struct nn_vec weights;
+  struct nn_vec errors;
+  struct nn_vec cache;
+  void (*forward)(struct nn_layer *l, int training);
+  void (*backward)(struct nn_layer *l, float *e, float rate);
+};
+
+#define NN_LAYER_EMPTY(l) ((l)->input.len * (l)->output.len == 0)
+
+#define NN_EMPTY()                                                             \
+  { 0 }
+
+#define NN_DENSE(out, in, flags)                                               \
+  {                                                                            \
+    (flags), {(out), NULL}, {(in), NULL}, {(((out) + 1) * (in)), NULL},        \
+	{(out), NULL}, {0, NULL}, nn_dense_forward, nn_dense_backward          \
+  }
+
+void nn_print(struct nn_layer *layers) {
+  int i, j;
+  struct nn_layer *l = layers;
+  for (i = 0; !NN_LAYER_EMPTY(l); l++, i++) {
+    printf("LAYER %d\n", i);
+    printf("  X (%p): ", (void *)l->input.data);
+    if (l->input.data != NULL) {
+      for (j = 0; j < l->input.len; j++) {
+	printf("%.02f ", l->input.data[j]);
+      }
+    }
+    printf("\n");
+    printf("  Y (%p): ", (void *)l->output.data);
+    for (j = 0; j < l->output.len; j++) {
+      printf("%.02f ", l->output.data[j]);
+    }
+    printf("\n");
+    printf("  W (%p): ", (void *)l->weights.data);
+    for (j = 0; j < l->weights.len; j++) {
+      printf("%.02f ", l->weights.data[j]);
+    }
+    printf("\n");
+    printf("  E (%p): ", (void *)l->errors.data);
+    for (j = 0; j < l->errors.len; j++) {
+      printf("%.02f ", l->errors.data[j]);
+    }
+    printf("\n");
+    printf("  C (%p): ", (void *)l->cache.data);
+    for (j = 0; j < l->cache.len; j++) {
+      printf("%.02f ", l->cache.data[j]);
+    }
+    printf("\n");
+  }
+}
+
+size_t nn_init(struct nn_layer *layers, const void *mem, size_t memsz) {
+  size_t expected = 0;
+  size_t last_outputs = 0;
+  float *prev_input = NULL;
+  struct nn_layer *l = layers;
+  struct nn_vec vec = {0, NULL};
+  vec.data = (float *)mem;
+  vec.len = memsz / sizeof(float);
+  for (; !NN_LAYER_EMPTY(l); l++) {
+    /* Remember number of outputs, network needs to store errors for the last
+     * layer in a vector of the same size */
+    last_outputs = l->output.len;
+    /* Connect layer input */
+    l->input.data = prev_input;
+    /* Connect layer output */
+    if (expected + l->output.len < vec.len) {
+      l->output.data = vec.data + expected;
+      prev_input = l->output.data;
+    }
+    expected = expected + l->output.len;
+    /* Prepare layer weights */
+    if (expected + l->weights.len < vec.len) {
+      l->weights.data = vec.data + expected;
+    }
+    expected = expected + l->weights.len;
+    /* Prepare layer errors */
+    if (expected + l->errors.len < vec.len) {
+      l->errors.data = vec.data + expected;
+    }
+    expected = expected + l->errors.len;
+    /* Prepare layer cache */
+    if (expected + l->cache.len < vec.len) {
+      l->cache.data = vec.data + expected;
+    }
+    expected = expected + l->cache.len;
+  }
+  expected = expected + last_outputs;
+  return expected * sizeof(float);
+}
+
+float *nn_predict(struct nn_layer *layers, float *x) {
+  struct nn_layer *l = layers;
+  l->input.data = x;
+  for (; !NN_LAYER_EMPTY(l); l++) {
+    l->forward(l, 0);
+  }
+  return (l - 1)->output.data;
+}
+
+float nn_train(struct nn_layer *layers, float *x, float *y, float rate) {
+  int i;
+  float *e;
+  float error = 0;
+  struct nn_layer *l = layers;
+  l->input.data = x;
+  for (; !NN_LAYER_EMPTY(l); l++) {
+    l->forward(l, 0);
+  }
+  l--;
+  e = l->cache.data + l->cache.len;
+  for (i = 0; i < l->output.len; i++) {
+    e[i] = y[i] - l->output.data[i];
+    error = error + (e[i] * e[i]) / l->output.len;
+  }
+  do {
+    l->backward(l, e, rate);
+    e = l->errors.data;
+  } while (l-- != layers);
+  return error; 
+}
+
+static void nn_dense_forward(struct nn_layer *l, int training) {
+  int i, j;
+  int n = l->input.len + 1;
+  /* Sum the inputs multiplied by weights */
+  for (i = 0; i < l->output.len; i++) {
+    float sum = 0;
+    /* This loop is likely to be vectorized */
+    for (j = 0; j < l->input.len; j++) {
+      sum = sum + l->input.data[j] * l->weights.data[i * n + j];
+    }
+    if ((l->flags & NN_FLAG_NO_BIAS) == 0) {
+      sum = sum + l->weights.data[i * n + n - 1];
+    }
+    l->output.data[i] = sum;
+  }
+  /* Apply activation function, for NN_FLAG_LINEAR do nothing */
+  switch (l->flags & NN_ACTFN) {
+  case NN_FLAG_SIGMOID:
+    for (i = 0; i < l->output.len; i++) {
+      l->output.data[i] = 1.0 / (1.0 + exp(-l->output.data[i]));
+    }
+    break;
+  case NN_FLAG_RELU:
+    /* This loop is likely to be vectorized */
+    for (i = 0; i < l->output.len; i++) {
+      l->output.data[i] = l->output.data[i] * (l->output.data[i] > 0);
+    }
+    break;
+  }
+}
+
+#define NN_DACT_SIGM(x) ((x) * (1 - (x)))
+#define NN_DACT_RELU(x) (1.0 * ((x) > 0))
+#define NN_DACT_LINEAR(x) 1.0
+
+static void nn_dense_backward(struct nn_layer *l, float *e, float rate) {
+  int i, j;
+  int n = l->input.len + 1;
+  for (j = 0; j < l->input.len; j++) {
+    float sum_e = 0;
+    /* These loops are likely to be vectorized */
+    switch (l->flags & NN_ACTFN) {
+    case NN_FLAG_SIGMOID:
+      for (i = 0; i < l->output.len; i++) {
+	sum_e = sum_e + e[i] * NN_DACT_SIGM(l->output.data[i]) *
+			    l->weights.data[i * n + j];
+      }
+      break;
+    case NN_FLAG_RELU:
+      for (i = 0; i < l->output.len; i++) {
+	sum_e = sum_e + e[i] * NN_DACT_RELU(l->output.data[i]) *
+			    l->weights.data[i * n + j];
+      }
+      break;
+    case NN_FLAG_LINEAR:
+      for (i = 0; i < l->output.len; i++) {
+	sum_e = sum_e + e[i] * NN_DACT_LINEAR(l->output.data[i]) *
+			    l->weights.data[i * n + j];
+      }
+      break;
+    }
+    l->errors.data[j] = sum_e;
+  }
+
+  for (i = 0; i < l->output.len; i++) {
+    float dsigm = 0;
+    switch (l->flags & NN_ACTFN) {
+    case NN_FLAG_SIGMOID:
+      dsigm = NN_DACT_SIGM(l->output.data[i]);
+      break;
+    case NN_FLAG_RELU:
+      dsigm = NN_DACT_RELU(l->output.data[i]);
+      break;
+    case NN_FLAG_LINEAR:
+      dsigm = NN_DACT_LINEAR(l->output.data[i]);
+      break;
+    }
+    /* This loop is likely to be vectorized */
+    for (j = 0; j < l->input.len; j++) {
+      l->weights.data[i * n + j] += rate * e[i] * dsigm * l->input.data[j];
+    }
+    if ((l->flags & NN_FLAG_NO_BIAS) == 0) {
+      l->weights.data[i * n + n - 1] += rate * e[i] * dsigm;
+    }
+  }
+}
+
+#endif /* NN_H */
---
+++ c/nn_test.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include "nn.h"
+
+int status = 0;
+
+#define ASSERT_EQ(a, b)                                                        \
+  do {                                                                         \
+    if (fabs(a - b) > 0.001) {                                                 \
+      printf("FAIL:%d: %f != %f\n", __LINE__, a, b);                           \
+      status = 1;                                                              \
+    }                                                                          \
+  } while (0)
+
+static void test_network_forward() {
+  float x[3];
+  float *z;
+  float mem[64];
+  struct nn_layer nn[] = {
+      NN_DENSE(3, 1, NN_FLAG_SIGMOID),
+      NN_EMPTY(),
+  };
+  nn_init(nn, mem, sizeof(mem));
+  nn[0].weights.data[0] = 1.74481176;
+  nn[0].weights.data[1] = -0.7612069;
+  nn[0].weights.data[2] = 0.3190391;
+  nn[0].weights.data[3] = -0.24937038;
+  x[0] = 1.62434536;
+  x[1] = -0.52817175;
+  x[2] = 0.86540763;
+  z = nn_predict(nn, x);
+  ASSERT_EQ(z[0], 0.96313579);
+  x[0] = -0.61175641;
+  x[1] = -1.07296862;
+  x[2] = -2.3015387;
+  z = nn_predict(nn, x);
+  ASSERT_EQ(z[0], 0.22542973);
+}
+
+static void test_network_backward() {
+  float x[2];
+  float y[2];
+  float *z;
+  float e;
+  float mem[1024];
+  struct nn_layer nn[] = {
+      NN_DENSE(2, 2, NN_FLAG_SIGMOID),
+      NN_DENSE(2, 2, NN_FLAG_SIGMOID),
+      NN_EMPTY(),
+  };
+  nn_init(nn, mem, sizeof(mem));
+  nn[0].weights.data[0] = 0.15;
+  nn[0].weights.data[1] = 0.2;
+  nn[0].weights.data[2] = 0.35;
+  nn[0].weights.data[3] = 0.25;
+  nn[0].weights.data[4] = 0.3;
+  nn[0].weights.data[5] = 0.35;
+
+  nn[1].weights.data[0] = 0.4;
+  nn[1].weights.data[1] = 0.45;
+  nn[1].weights.data[2] = 0.6;
+  nn[1].weights.data[3] = 0.5;
+  nn[1].weights.data[4] = 0.55;
+  nn[1].weights.data[5] = 0.6;
+
+  x[0] = 0.05;
+  x[1] = 0.1;
+  z = nn_predict(nn, x);
+  ASSERT_EQ(z[0], 0.75136507);
+  ASSERT_EQ(z[1], 0.772928465);
+  y[0] = 0.01;
+  y[1] = 0.99;
+  e = nn_train(nn, x, y, 0);
+  ASSERT_EQ(e, 0.298371109);
+  nn_train(nn, x, y, 0.5);
+  nn_print(nn);
+  ASSERT_EQ(nn[1].weights.data[0], 0.35891);
+  ASSERT_EQ(nn[1].weights.data[1], 0.40866);
+  ASSERT_EQ(nn[1].weights.data[2], 0.53075);
+  ASSERT_EQ(nn[1].weights.data[3], 0.5113);
+  ASSERT_EQ(nn[1].weights.data[4], 0.5613);
+  ASSERT_EQ(nn[1].weights.data[5], 0.61904);
+
+  ASSERT_EQ(nn[0].weights.data[0], 0.14978);
+  ASSERT_EQ(nn[0].weights.data[1], 0.19956);
+  ASSERT_EQ(nn[0].weights.data[2], 0.34561);
+  ASSERT_EQ(nn[0].weights.data[3], 0.24975);
+  ASSERT_EQ(nn[0].weights.data[4], 0.2995);
+  ASSERT_EQ(nn[0].weights.data[5], 0.34502);
+}
+
+int main() {
+  (void) test_network_forward;
+  /*test_network_forward();*/
+  test_network_backward();
+  return status;
+}
