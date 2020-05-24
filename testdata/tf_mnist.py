04993ef0284e434e21e4fbc52d6aff4821cd1ff6
Serge Zaitsev <zaitsev.serge@gmail.com>
2020-05-24 13:44:00 +0200

    add mnist example

--- testdata/tf_mnist.py
+++ testdata/tf_mnist.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import tensorflow as tf
+import numpy as np
 import json
 import csv
 
@@ -9,16 +10,27 @@ import csv
 x_train = x_train.astype("float32") / 255
 x_test = x_test.astype("float32") / 255
 
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+imggen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    zoom_range=0.2,
+    shear_range=10,
+)
+imggen.fit(x_train)
+
+leaky_relu = lambda x: tf.nn.leaky_relu(x, 0.01)
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(units=36, activation=tf.nn.leaky_relu))
-model.add(tf.keras.layers.Dense(units=14, activation=tf.nn.leaky_relu))
+model.add(tf.keras.layers.Dense(units=36, activation=leaky_relu))
+model.add(tf.keras.layers.Dense(units=14, activation=leaky_relu))
 model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
 
 model.compile(
     optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
 )
-model.fit(x_train, y_train, epochs=20)
+model.fit(imggen.flow(x_train, y_train, batch_size=32), epochs=400)
 model.evaluate(x_test, y_test)
 
 # Save model
@@ -26,7 +38,7 @@ model.save("mnist.h5")
 
 # Load model
 # model = tf.keras.models.load_model(
-    # "mnist.h5", custom_objects={"leaky_relu": tf.nn.leaky_relu}
+# "mnist.h5", custom_objects={"leaky_relu": tf.nn.leaky_relu}
 # )
 
 W = model.get_weights()
